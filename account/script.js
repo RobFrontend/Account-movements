0a671d07376510e119688afd8a01c963a1551418
Robert <130874846+RobFrontend@users.noreply.github.com>
2023-11-03 17:46:08 +0100

    Add files via upload

--- account/script.js
+++ account/script.js
@@ -12,24 +12,30 @@
 // EXAMPLE
 const movements = [3400, -200, -750, 100, -200, 50, -120];
 
+console.log(`---- Deposits ----`);
 const deposits = movements.filter((mov) => mov > 0);
 console.log(deposits);
 
-const depositsTotal = deposits.reduce((acc, dep) => acc + dep);
+console.log(`---- TOTAL Deposits ----`);
+const depositsTotal = deposits.reduce((acc, dep) => acc + dep, 0);
 console.log(depositsTotal);
 
+console.log(`---- Charges ----`);
 const charges = movements.filter((mov) => mov < 0);
 console.log(charges);
 
-const chargesTotal = charges.reduce((acc, char) => acc + char);
+console.log(`---- TOTAL Charges ----`);
+const chargesTotal = charges.reduce((acc, char) => acc + char, 0);
 console.log(chargesTotal);
 
+console.log(`---- Account balance after all movements ----`);
+console.log(`---- (There was $1300 before payday) ----`);
 const movAndBal = function () {
   movements.map((mov, i) =>
-    console.log(`${mov > 0 ? `Deposit` : `Charge`} of $${mov}`)
+    console.log(`${mov > 0 ? `Deposit` : `Charge`} of $${Math.abs(mov)}`)
   );
 
   const balance = movements.reduce((acc, mov) => acc + mov, 1300);
-  console.log(balance);
+  console.log(`There is $${balance} in the account now!!!`);
 };
 movAndBal();
